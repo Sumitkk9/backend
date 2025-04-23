215fc9b7caa73834c23b88b30ad1d492b061cf3d
sumitkk135 <sumitkk135@gmail.com>
2025-04-24 01:33:09 +0530

    new check

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello World' });
+import { Chat } from "../model/models/chat.modal";
+
+export default async function handler(req, res) {
+
+  const data = Chat.find()
+    res.status(200).json({ message: 'Hello World',data });
   }
