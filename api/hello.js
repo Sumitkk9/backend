d2f138a8a4236d78b240faf68d6c823745a5c143
sumitkk135 <sumitkk135@gmail.com>
2025-04-24 01:53:08 +0530

    final check

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-import { Chat } from "../model/models/chat.modal";
-
-export default async function handler(req, res) {
-
-  const data = Chat.find()
-    res.status(200).json({ message: 'Hello World',data });
-  }
